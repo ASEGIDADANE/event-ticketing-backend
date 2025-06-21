234b6c2c78efd96a940f10aede314b57846d4b90
ASEGIDADANE <asegidadane27@gmail.com>
2025-06-21 18:20:24 +0300

    bookings controller is done

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,6 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { BookingsService } from './bookings.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { User } from 'generated/prisma';
+import { CreateBookingDto } from './dto/create-booking.dto';
+import { currentUser } from 'src/auth/decorators/current-user.decorator';
 
 @Controller('bookings')
 export class BookingsController {
-    
+    constructor(private readonly bookingsService: BookingsService) {}
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async createBooking(
+        @Body() createBookingDto: CreateBookingDto,
+        @currentUser() user: User,
+    ) {
+        return this.bookingsService.createBooking(createBookingDto, user.id);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('me')
+    async getMyBookings(@currentUser() user: User) {
+        return this.bookingsService.getUserBookings(user.id);
+    }
 }
