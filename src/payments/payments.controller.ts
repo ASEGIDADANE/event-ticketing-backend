b70250654d2ca80080567de7a94c53db3e995398
ASEGIDADANE <asegidadane27@gmail.com>
2025-06-22 09:19:41 +0300

    payment controller is done

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Req } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { CreatePaymentDto } from './dto/create-payment.dto';
+import { RequestWithUser } from 'src/auth/interfaces/request.interface';
 
 @Controller('payments')
-export class PaymentsController {}
+export class PaymentsController {
+    constructor(private readonly paymentsService: PaymentsService) {}
+
+    async createPayment(dto: CreatePaymentDto, @Req() req: RequestWithUser) {
+        const userId = req.user.id;
+        return this.paymentsService.createPayment(dto, userId);
+    }
+}
