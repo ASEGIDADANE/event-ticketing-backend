b1aa4069f7abc17af81ad7fd5d7e5f9244f29689
ASEGIDADANE <asegidadane27@gmail.com>
2025-07-01 17:27:49 +0300

    impl pagination and caching

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,15 +1,35 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 import { NotFoundException } from '@nestjs/common/exceptions/not-found.exception';
 import { CreateEventDto } from './create-event.dto';
 import { FilterEventsDto } from './dto/filter-events.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { Cache, CACHE_MANAGER} from '@nestjs/cache-manager';
+import { FindEventQueryDto } from './dto/find-event-query.dto';
+import { promises } from 'dns';
+import { paginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { PaginationResponse } from 'src/common/interfaces/pagination-response.interface';
+import { Event as EventEntity } from '../../generated/prisma';
+
+
 
 
 @Injectable()
 export class EventsService {
-    constructor(private prisma: PrismaService) {}
+    private eventsCacheKey:Set<string> = new Set();
+    constructor(
+        private prisma: PrismaService,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache
+    ) {}
+
+
+    private eventListCacheKey (query:FindEventQueryDto): string {
+        const { page, limit,name } = query;
+        return `events:${name || ''}:page:${page || 1}:limit:${limit || 10}`;
+    }
+
+    
     async createEvent(createEventDto: CreateEventDto, userId: string) {
         const { name, description, date, venueId } = createEventDto;
 
@@ -37,39 +57,102 @@ export class EventsService {
   }
 
 
+  async getAllEvents(query: FindEventQueryDto): Promise<PaginationResponse<EventEntity>> {
+    const cacheKey = this.eventListCacheKey(query);
+    this.eventsCacheKey.add(cacheKey);
+    const cachedEvents = await this.cacheManager.get<PaginationResponse<EventEntity>>(cacheKey);
 
-  async getAllEvents(filterDto: FilterEventsDto) {
-    const { location, fromDate, toDate, isPublished } = filterDto;
-
-    const filters: any = {};
-
-    if (isPublished !== undefined) {
-        filters.isPublished = isPublished === 'true';
+    if (cachedEvents) {
+      console.log('cache hit');
+      return cachedEvents;
     }
 
-    if (fromDate || toDate) {
-        filters.date = {};
-        if (fromDate) filters.date.gte = new Date(fromDate);
-        if (toDate) filters.date.lte = new Date(toDate);
+    console.log('cache miss');
+
+    const { page = 1, limit = 10, name } = query;
+
+    const skip = (page - 1) * limit;
+ 
+
+    const where: any = {};
+    if (name) {
+      where.name = { contains: name, mode: 'insensitive' };
     }
 
-    if (location) {
-        filters.venue = {
-        location: {
-            contains: location,
-            mode: 'insensitive',
-        },
-        };
-    }
-
-    return this.prisma.event.findMany({
-        where: filters,
+    const [events, total] = await Promise.all([
+      this.prisma.event.findMany({
+        where,
         include: {
-        venue: true,
-        organizer: true,
+          venue: true,
+          organizer: true,
         },
-    });
+        skip,
+        take: limit,
+        orderBy: { date: 'desc' },
+      }),
+      this.prisma.event.count({ where }),
+    ]);
+
+    const result: PaginationResponse<EventEntity> = {
+      items: events as EventEntity[],
+      metadata: {
+        currentPage: page,
+        totalPages: Math.ceil(total / limit),
+        totalItems: total,
+        itemsPerPage: limit,
+        hasNextPage: page < Math.ceil(total / limit),
+        hasPreviousPage: page > 1,
+      },
+};
+
+    await this.cacheManager.set(cacheKey, result, 60); // cache for 60 seconds
+
+    return result;
+      
+
+
+
+
+
+
 }
+
+
+
+//   async getAllEvents(filterDto: FilterEventsDto) {
+//     const { location, fromDate, toDate, isPublished } = filterDto;
+
+//     const filters: any = {};
+
+//     if (isPublished !== undefined) {
+//         filters.isPublished = isPublished === 'true';
+//     }
+
+//     if (fromDate || toDate) {
+//         filters.date = {};
+//         if (fromDate) filters.date.gte = new Date(fromDate);
+//         if (toDate) filters.date.lte = new Date(toDate);
+//     }
+
+//     if (location) {
+//         filters.venue = {
+//         location: {
+//             contains: location,
+//             mode: 'insensitive',
+//         },
+//         };
+//     }
+
+//     return this.prisma.event.findMany({
+//         where: filters,
+//         include: {
+//         venue: true,
+//         organizer: true,
+//         },
+//     });
+// }
+
+
 
 async getEventById(id: string) {
   const event = await this.prisma.event.findUnique({
